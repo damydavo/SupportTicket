45c121f36e9341daeb37e56fc1f076eae4c39afb
unknown <damydavo2015@gmail.com>
2022-10-20 12:31:55 +0100

    Error handling and exception logics

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,21 @@
+const asyncHandler = require('express-async-handler')
+
 //@desc Register a new user
 //@route  /api/users
 //@access  public
 
-const registerUser = ((req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    //validation
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please include all fields')
+    }
     res.send('Register User')
 })
 
-const loginUser = ((req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login User')
 })
 
